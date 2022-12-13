1136171481cc70dcc3d9932f8ba7c52db33c6633
MarselMingaripov <bard1812@gmail.com>
2022-12-13 18:49:38 +0300

    выполнил задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+//        task2();
+//        task3();
+//        task4();
+    }
+
+    public static int[] generateRandomArray() {
+        Random random = new Random();
+        int[] array = new int[30];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100000) + 100000;
+        }
+        return array;
+    }
+
+    public static void task1() {
+        System.out.println("task 1");
+
+        int[] expenceArray = generateRandomArray();
+        int sum = 0;
+        for (int element : expenceArray) {
+            sum += element;
+        }
+        System.out.printf("«Сумма трат за месяц составила %d рублей»", sum);
+        System.out.println();
     }
 }
